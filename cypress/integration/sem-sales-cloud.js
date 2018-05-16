7f7980420ae14068b38678df234e182462265c0a
Sebastian Busch <sbusch@salesforce.com>
2018-05-16 20:32:02 +0200

    Added first SEM Sales Cloud page test - test if rendering

---
+++ cypress/integration/sem-sales-cloud.js
@@ -0,0 +1,22 @@
+describe('These are the tests for the EMEA core Sales Cloud SEM pages', () => {
+    
+    const locales = ['eu', 'fr', 'uk', 'de', 'nl'];
+    for (let i = 0; i < locales.length; ++i) {
+        before(function() {
+            // Visiting our app before each test removes any state build up from
+            // previous tests. Visiting acts as if we closed a tab and opened a fresh one
+            cy.visit('https://www.salesforce.com/' + locales[i] + '/campaign/sem/sales-cloud/');
+        })
+       //page renders
+        it('should not be a 404 page', function() {
+            cy.title().should('not.include', '404')
+        })
+     }
+   
+    
+})
+
+        
+
+     
+
